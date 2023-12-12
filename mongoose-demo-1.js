bffcc3f7572e293a978a754c744d174024832671
Eric Zhou <ericzhou1212@gmail.com>
2023-12-11 18:51:04 -0800

    completed

--- mongoose-demo-1.js
+++ mongoose-demo-1.js
@@ -9,3 +9,12 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   // we're connected!
 });
+
+const fruitSchema = new mongoose.Schema({
+  name: String
+});
+
+const Fruit = mongoose.model('Fruit', fruitSchema);
+
+const apple = new Fruit({ name: 'apple' });
+console.log(apple.name); // 'apple'
--- mongoose-demo-2.js
+++ mongoose-demo-2.js
@@ -10,4 +10,18 @@ db.once('open', function() {
   // we're connected!
 });
 
+const kittySchema = new mongoose.Schema({
+  name: String
+});
+
+kittySchema.methods.speak = function () {
+  const greeting = this.name
+    ? "Meow name is " + this.name
+    : "I don't have a name";
+  console.log(greeting);
+};
+
+const Kitten = mongoose.model('Kitten', kittySchema);
+const fluffy = new Kitten({ name: 'fluffy' });
+fluffy.speak(); // "Meow name is fluffy"
 
